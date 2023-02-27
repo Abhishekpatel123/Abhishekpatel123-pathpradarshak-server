ffdae4fcd89bb75acaa5ff6661215e4c16990bab
Abhishek <abhipatel8719@gmail.com>
2023-02-27 13:50:41 +0530

    FIX - spell

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+console.log(process.env, 'env')
 module.exports = {
   // mongoURI: "mongodb://spectre:spectre007@ds253537.mlab.com:53537/elearning", //apurva's
   // mongoURI: 'mongodb+srv://admin:admin@cluster0-xb8vd.gcp.mongodb.net/E-learning?retryWrites=true&w=majority',
