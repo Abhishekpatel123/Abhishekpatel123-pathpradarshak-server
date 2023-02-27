5038c853ca7a0b4cf94498fe524f4a93812448c4
Abhishek <abhipatel8719@gmail.com>
2023-02-27 13:49:07 +0530

    FIX - spell

--- config/keys.js
+++ config/keys.js
@@ -3,6 +3,6 @@ module.exports = {
   // mongoURI: "mongodb://spectre:spectre007@ds253537.mlab.com:53537/elearning", //apurva's
   // mongoURI: 'mongodb+srv://admin:admin@cluster0-xb8vd.gcp.mongodb.net/E-learning?retryWrites=true&w=majority',
   mongoURI: process.env.MONGO_URL,
-  secretOrKey: process.env.JWt_SECRET,
+  secretOrKey: process.env.JWT_SECRET,
   adminSecretOrKey: process.env.ADMIN_JWT_SECRET,
 };
